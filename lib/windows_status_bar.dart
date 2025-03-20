940db33d1222d8aa9df5a52fc97bbcab8c55b8d3
Tran Huu Dang <96557038+tranhuudang@users.noreply.github.com>
2025-03-20 22:43:37 +0700

    fix: hide default windows status bar

--- lib/windows_status_bar.dart
+++ lib/windows_status_bar.dart
@@ -1,11 +1,15 @@
 library windows_status_bar;
+
+import 'dart:io';
+
 import 'package:window_manager/window_manager.dart';
 export 'windows_status_bar_widget.dart';
 
-class WindowsStatusBar{
-  static initialize() async{
+class WindowsStatusBar {
+  static initialize() async {
     await windowManager.ensureInitialized();
+    if (Platform.isWindows) {
+      windowManager.setTitleBarStyle(TitleBarStyle.hidden);
+    }
   }
-
 }
-
